55978c559e10624ab8ad0cd447db5438bd8693ab
phuong_ptt <phuong_ptt@septeni-technology.jp>
2020-09-04 16:11:09 +0700

    init database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('user_id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('api_token', 64);
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
